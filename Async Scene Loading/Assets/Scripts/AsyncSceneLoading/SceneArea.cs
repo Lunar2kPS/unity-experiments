059d9f7c7c45d33aab5e5907216321b9a876874f
Carlos DaLomba <carlos@cometpeakgames.net>
2022-07-12 09:20:38 -0400

    1st iteration of async scene loading!

---
+++ Async Scene Loading/Assets/Scripts/AsyncSceneLoading/SceneArea.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+namespace AsyncSceneLoading {
+    /// <summary>
+    /// Defines a scene that gets automatically loaded when object(s) of interest enter a certain area.
+    /// </summary>
+    [CreateAssetMenu]
+    public class SceneArea : ScriptableObject {
+        [Tooltip("The asset path of the scene to load when an object of interest enters the given area.")]
+        [SerializeField] private string scenePath;
+
+        [Tooltip("Defines the volume in world-space that triggers the automatic loading of this scene.\n" +
+            "When the object(s) of interest leave this area, the scene will be unloaded.")]
+        [SerializeField] private Bounds bounds;
+
+        public string ScenePath => scenePath;
+        public Bounds Bounds => bounds;
+    }
+}
---
+++ Async Scene Loading/Assets/Scripts/AsyncSceneLoading/SceneAreaLoader.cs
@@ -0,0 +1,62 @@
+﻿using System.Linq;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace AsyncSceneLoading {
+    public class SceneAreaLoader : MonoBehaviour {
+        [SerializeField] private Transform[] objectsOfInterest;
+        [SerializeField] private SceneArea[] areas;
+
+        private HashSet<string> inProgressLoads = new HashSet<string>();
+
+        public IEnumerable<Transform> ObjectsOfInterest => objectsOfInterest.Where(obj => obj != null);
+        public IEnumerable<SceneArea> Areas => areas.Where(a => a != null);
+
+        #region Unity Messages
+        private void OnDrawGizmosSelected() {
+            foreach (SceneArea area in Areas) {
+                Bounds bounds = area.Bounds;
+                Gizmos.DrawWireCube(bounds.center, bounds.size);
+            }
+        }
+
+        private void Update() {
+            //NOTE: Not fully optimized due to all the Transform.position calls
+            foreach (SceneArea area in Areas) {
+                if (IsInProgress(area))
+                    continue;
+
+                string scenePath = area.ScenePath;
+                bool shouldBeLoaded = ContainsAnyObjectsOfInterest(area);
+                bool isLoaded = SceneManager.GetSceneByPath(scenePath).IsValid();
+
+                if (isLoaded != shouldBeLoaded) {
+                    if (shouldBeLoaded) {
+                        inProgressLoads.Add(scenePath);
+                        SceneManager.LoadSceneAsync(scenePath, LoadSceneMode.Additive).completed += (AsyncOperation a) => {
+                            inProgressLoads.Remove(scenePath);
+                        };
+                    } else {
+                        inProgressLoads.Add(scenePath);
+                        SceneManager.UnloadSceneAsync(scenePath).completed += (AsyncOperation a) => {
+                            inProgressLoads.Remove(scenePath);
+                        };
+                    }
+                }
+            }
+        }
+        #endregion
+
+        private bool ContainsAnyObjectsOfInterest(SceneArea area) {
+            Bounds b = area.Bounds;
+            foreach (Transform obj in ObjectsOfInterest)
+                if (b.Contains(obj.position))
+                    return true;
+            return false;
+        }
+
+        private bool IsInProgress(SceneArea area) => area != null && IsInProgress(area.ScenePath);
+        private bool IsInProgress(string scenePath) => inProgressLoads.Contains(scenePath);
+    }
+}

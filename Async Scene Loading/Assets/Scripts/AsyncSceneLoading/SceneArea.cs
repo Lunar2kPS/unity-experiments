e5f99e9755493cab77612873d40a01906db006a2
Devon Hood <devon_hood@bloomfield.edu>
2022-08-22 23:54:04 -0400

    Learning Your Code Carlos
    - Comments to see if I understand the code.

--- Async Scene Loading/Assets/Scripts/AsyncSceneLoading/SceneArea.cs
+++ Async Scene Loading/Assets/Scripts/AsyncSceneLoading/SceneArea.cs
@@ -7,11 +7,11 @@ namespace AsyncSceneLoading {
     [CreateAssetMenu]
     public class SceneArea : ScriptableObject {
         [Tooltip("The asset path of the scene to load when an object of interest enters the given area.")]
-        [SerializeField] private string scenePath;
+        [SerializeField] private string scenePath; //The scene to load when the player enters the bounds area. 
 
         [Tooltip("Defines the volume in world-space that triggers the automatic loading of this scene.\n" +
             "When the object(s) of interest leave this area, the scene will be unloaded.")]
-        [SerializeField] private Bounds bounds;
+        [SerializeField] private Bounds bounds; //The area in which when the player enters the scene loads: Also is shown in the scene view when using debugging. ;)
 
         public string ScenePath => scenePath;
         public Bounds Bounds => bounds;

87bfd44ad19beb0e3e2bcf6c6efd1e6aa103f3f9
Carlos DaLomba <carlos@lookingglassfactory.com>
2024-06-28 12:05:52 -0500

    First UDP tests...

--- UDP Networking/Assets/Scripts/UDPNetworking/UDPTester.cs
+++ UDP Networking/Assets/Scripts/UDPNetworking/UDPTester.cs
@@ -1,7 +1,120 @@
+using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 namespace UDPNetworking {
     public class UDPTester : MonoBehaviour {
+        [Serializable]
+        public struct SerializableIPEndpoint {
+            public string address;
+            public ushort port;
 
+            public IPEndPoint ToEndpoint() {
+                return new IPEndPoint(
+                    IPAddress.Parse(address),
+                    port
+                );
+            }
+        }
+
+        [Serializable]
+        public struct MessageData {
+            public string dateTime;
+            public string text;
+
+            public MessageData(string text) {
+                dateTime = DateTime.Now.Ticks.ToString();
+                this.text = text;
+            }
+
+            public static MessageData FromBytes(byte[] data) {
+                string json = Encoding.UTF8.GetString(data);
+                return JsonUtility.FromJson<MessageData>(json);
+            }
+
+            public byte[] ToBytes() {
+                string json = JsonUtility.ToJson(this);
+                return Encoding.UTF8.GetBytes(json);
+            }
+        }
+
+        [SerializeField] private SerializableIPEndpoint receiverEndpoint;
+        [SerializeField] private SerializableIPEndpoint senderEndpoint;
+
+        private UdpClient client;
+        private IPEndPoint receiverEndpointInternal;
+        private IPEndPoint senderEndpointInternal;
+
+        private CancellationTokenSource cancellation;
+        private CancellationToken cancelToken;
+        private Task receiveTask;
+
+        private Keyboard keyboard;
+
+        private void OnEnable() {
+            receiverEndpointInternal = receiverEndpoint.ToEndpoint();
+            senderEndpointInternal = senderEndpoint.ToEndpoint();
+            client = new UdpClient(receiverEndpointInternal);
+
+            keyboard = InputSystem.GetDevice<Keyboard>();
+            cancellation = new();
+            cancelToken = cancellation.Token;
+            receiveTask = Task.Run(ReceiveLoop);
+        }
+
+        private void OnDisable() {
+            cancellation.Cancel();
+            try {
+                receiveTask.Wait(1000);
+            } catch { }
+
+            if (client != null) {
+                client.Dispose();
+                client = null;
+            }
+        }
+
+        private void Update() {
+            if (keyboard != null) {
+                if (keyboard[Key.Space].wasPressedThisFrame) {
+                    byte[] bytes = new MessageData("Spacebar was pressed!").ToBytes();
+                    client.Send(bytes, bytes.Length, senderEndpointInternal);
+                }
+            }
+        }
+
+        private async Task ReceiveLoop() {
+            try {
+                Task<UdpReceiveResult> cancelTask = Task.Run(async () => {
+                    await cancelToken;
+                    return default(UdpReceiveResult);
+                });
+                while (true) {
+                    cancelToken.ThrowIfCancellationRequested();
+
+                    Task<UdpReceiveResult> receiveTask = client.ReceiveAsync();
+                    Task<Task<UdpReceiveResult>> current = Task.WhenAny(receiveTask, cancelTask);
+
+                    await current;
+                    Task<UdpReceiveResult> completedTask = current.Result;
+                    if (completedTask == receiveTask) {
+                        byte[] data = receiveTask.Result.Buffer;
+                        MessageData message = MessageData.FromBytes(data);
+                        Debug.Log("RECEIVED! " + data.Length + " B:\n" + message.dateTime + " -- " + message.text);
+                    }
+                }
+            } catch (OperationCanceledException e) {
+                Debug.Log("CANCELLED by " + e.GetType().Name + "!");
+                throw;
+            } catch (Exception e) {
+                Debug.LogException(e);
+                throw;
+            }
+        }
     }
 }
---
+++ UDP Networking/Assets/Scripts/UDPNetworking/Utility/CancellationTokenExtensions.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Threading;
+using System.Runtime.CompilerServices;
+
+namespace UDPNetworking {
+    public static class CancellationTokenExtensions {
+        /// <summary>
+        /// Allows a cancellation token to be awaited.
+        /// </summary>
+        public static CancellationTokenAwaiter GetAwaiter(this CancellationToken token) {
+            return new CancellationTokenAwaiter {
+                token = token
+            };
+        }
+
+        /// <summary>
+        /// The custom awaiter for cancellation tokens.
+        /// </summary>
+        public struct CancellationTokenAwaiter : INotifyCompletion, ICriticalNotifyCompletion {
+            internal CancellationToken token;
+
+            public CancellationTokenAwaiter(CancellationToken token) {
+                this.token = token;
+            }
+
+            //NOTE: This is called by compiler-generated/.NET internals to check if the Task has completed.
+            public bool IsCompleted => token.IsCancellationRequested;
+
+            //NOTE: This is called by compiler-generated methods, when the Task has completed.
+            //Instead of returning a result, we throw an exception (to match .NET behavior).
+            public object GetResult() {
+                if (IsCompleted)
+                    throw new OperationCanceledException();
+                else
+                    throw new InvalidOperationException("The cancellation token has not yet been cancelled.");
+            }
+
+            //NOTE: The compiler will generate stuff that hooks in here.
+            //We hook those methods back into the CancellationToken.
+            public void OnCompleted(Action continuation) => token.Register(continuation);
+            public void UnsafeOnCompleted(Action continuation) => token.Register(continuation);
+        }
+    }
+}
